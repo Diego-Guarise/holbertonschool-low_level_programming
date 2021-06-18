90c6ada798256ea32485f36f88449d13ad26bad8
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-17 17:41:13 -0700

    allcommitpush

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - display a text
+*
+ *Description: display "Programming is like
+ *building a multilingual puzzle using the puts function.
+ *Return: 0 when finish
+ */
+int main(void)
+{
+	printf "%s/n"("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
