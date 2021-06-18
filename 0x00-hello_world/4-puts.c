23d4ee7119594d51a10465f7ca414a2cfa754150
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-17 17:35:54 -0700

    allcommitpush

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *Description: display "Programming is like
  *building a multilingual puzzle using the puts function.
  *Return: 0 when finish
- */ 
-int main(void)	
-{	
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
