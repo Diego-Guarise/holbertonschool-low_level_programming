572e21a8c7c3744ce8aa2e2c37f74012dce734e4
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-17 18:11:52 -0700

    allcommitpush

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,16 @@
  */
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
+	int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longSize = sizeof(long int);
+	int longlongSize = sizeof(long long int);
+	int floatSize = sizeof(float);
 	
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of char: %zu bytes\n", sizeof(charSize));
+	printf("Size of int: %zu bytes\n", sizeof(intSize));
+	printf("Size of long int: %zu bytes\n", sizeof(longSize));
+	printf("Size of a float: %d byte(s)\n", sizeof(floatSize);
+	printf("Size of long long int: %zu byte\n", sizeof(longlongSize));
 	return (0);
 }
