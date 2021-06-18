abeb752946e5da7a5dcf9e281d2904fd3769063c
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-17 17:13:04 -0700

    allcommitpush

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h> /*Directivas de procesamiento*/
+int pin=0;	/*Declaraciones globales*/
+main()	/*Funcion principal*/
+{	
+	float x=6.1; /*Declaraciones locales*/
+	puts("\"Programming is like building a multilingual puzzle"); /*Instruccion*/	
+	return(0);
+}
