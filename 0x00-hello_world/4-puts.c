c35876cad9281f21e544c2c885b5c0a2f290aaf1
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-17 17:27:49 -0700

    allcommitpush

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
-#include <stdio.h> /*Directivas de procesamiento*/
-	/*Declaraciones globales*/
-int main(void)	/*Funcion principal*/
+#include <stdio.h>
+/** 
+* main - Descripción breve, una sola línea 
+* @parameterx: Descripción del parámetro x 
+(* una línea en blanco 
+* Descripción: Descripción más larga de la función)? 
+(* encabezado de la sección: descripción de la sección) * 
+* Return: descripción del valor devuelto 
+*/ 
+int main(void)	
 {	
-	 /*Declaraciones locales*/
-	puts("\"Programming is like building a multilingual puzzle"); /*Instruccion*/	
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
