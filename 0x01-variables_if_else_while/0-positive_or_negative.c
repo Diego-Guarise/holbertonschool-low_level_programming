56578939a1752fcebc15aad772e1e01b903aee9e
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-18 20:01:11 -0700

    addcommitpush

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-/*main - positive or negative*/
+/* main - Positive or Negative - print sign of random number */
 /* betty style doc for function main goes there */
 int main(void)
 {
