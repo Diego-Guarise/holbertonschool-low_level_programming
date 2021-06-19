53f26575164b044a235de1c54f1f4ddccf517a82
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-18 19:59:54 -0700

    addcommitpush

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
+/*main - positive or negative*/
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -10,7 +10,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
