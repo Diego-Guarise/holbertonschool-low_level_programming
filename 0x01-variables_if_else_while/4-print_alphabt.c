f4406f1aa0a7056be06722f9d4fce6375602232b
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-21 16:59:03 -0700

    addcommitpush

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - print alphabet deleted e and q
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letras;
+
+	for (letras = 'a'; letras <= 'z'; letras++)
+	{
+		if (letras != 'e' && letras != 'q')
+		{
+		putchar(letras);
+		putchar('\n');
+		}
+	}
+	return (0);
+}
