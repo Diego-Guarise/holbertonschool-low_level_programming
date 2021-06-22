f9602722f3de885801074af161c2d9292b42470f
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-22 13:18:45 -0700

    --all,commit,push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/* more headers goes there */
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letras;
+
+	for (letras = 'a'; letras <= 'z'; letras++)
+		_putchar(letras);
+	{
+		_putchar('\n');
+	}
+	return (0);
+}
