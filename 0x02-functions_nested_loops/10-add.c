b21fad5802590a0173bfb1b26725f580398e5899
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-22 19:52:32 -0700

    --all,commit,push

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - Write a function that adds two integers and returns the result
+ * @a: int b
+ * @b: int a
+ * Return: (a+b)
+ */
+int add(int a, int b);
+{
+	int c;
+	c = a + b
+	return (c);
+}
