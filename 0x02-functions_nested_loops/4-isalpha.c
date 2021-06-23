0ea8fdc268c1a94a0b45a15f3c430591b0f7d8d8
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-22 18:53:14 -0700

    --all,commit,push

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z') || if (c >= 'A' && c <= 'Z')
 		return (1);
