c97cfed73c32d5f25a981e4acb2a9f5a178036ac
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-22 18:49:16 -0700

    --all,commit,push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,8 @@
 #include "holberton.h"
 /**
- *
- * _islower - checks for lowercase character.
- * Return: Always 0 Success
- * @c: Character in question
- *
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
