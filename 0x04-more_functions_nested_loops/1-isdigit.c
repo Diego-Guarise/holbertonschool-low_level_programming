078691b2eb88caf5d56bee900e5e9ec201685922
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-24 09:14:45 -0700

    gitaddcommitpush

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,10 @@
 
 /**
  * _isdigit - checks for a digit (0 through 9).
- * @c: int to be check
- * Return: Returns 1 if c is a digit, 0 otherwise
+ * @c: the character to check
+ * Return: Returns 1 if c is a digit (0-9), 0 otherwise
  */
-
 int _isdigit(int c)
-
 {
 	if (c >= 0 && c <= 9)
 		return (1);
