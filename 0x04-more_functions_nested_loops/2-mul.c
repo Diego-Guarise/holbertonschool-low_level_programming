d954dfe5342d2c8757799a48752f84eb48d7b25d
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-24 09:04:29 -0700

    gitaddcommitpush

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul(int a, int b) - Write a function that multiplies two integers.
+ * @a: the character
+ * @b: the character
+ * Return: 0 
+ */
+
+int mul(int a, int b)
+
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
