75fdbb191738b9fa190265f27f72a5020b4554e6
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-24 08:23:52 -0700

    gitaddcommitpush

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _isupper(int c) - checks for uppercase character.
+ * @c: the character to check
+ * Return: Returns 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+    return (c >= 'A' && c <= 'Z')
+}
