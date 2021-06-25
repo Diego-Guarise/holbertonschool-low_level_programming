f629285ba422a31c0ff89396e4d2da946e359005
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-24 19:36:11 -0700

    gitaddcommitpush

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,10 +12,10 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0 && x % 3 != 0)
-			{
-				printf(" Fizz");
-			}
+		if (x % 3 == 0 && x % 5 != 0)
+		{
+			printf(" Fizz");
+		}
 		else if (x % 5 == 0 && x % 3 != 0)
 		{
 			printf (" Buzz");
