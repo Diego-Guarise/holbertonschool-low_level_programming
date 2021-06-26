2593a0a86d0b213c18ec17af7b0dbacbf14b1d6c
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-25 19:01:48 -0700

    gitaddpush

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,29 +1,9 @@
-#include "holberton.h"
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
+#include <stdio.h>
 
-int largest_number(int a, int b, int c)
-{
-    int largest;
+void positive_or_negative(int i);
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
+int largest_number(int a, int b, int c);
+#endif /* HOLBERTON_H */
