084bae9660d7871ae4e3bd6cc44832baac1890f6
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-28 10:52:11 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @b: variable
+ * @a: variable
+ * @c: variable
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+		
+	c = *a;
+	*a = *b;
+	*b = c;
+}
