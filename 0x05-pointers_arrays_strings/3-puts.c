e78c3bcc133fba12cfa648e138d0b048595fedc2
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-29 19:27:02 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <unistd.h>
 
 /**
  * _puts - prints a string to stdout
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,5 @@
-#include <stddef.h>
-#include <string.h>
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,3 +12,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 size_t strlen(const char *s);
+
+#endif
