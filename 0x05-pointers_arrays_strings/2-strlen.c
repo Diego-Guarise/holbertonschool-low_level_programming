75cedd3ac86a89895884d436d4b5fa19bd80882f
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-29 17:56:50 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include <string.h>
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * return - strlen s
+ * @s: variable
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
