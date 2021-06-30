891527b623ee5cfcf22c701c12acd81678989f2f
Diego-Guarise <l.guariselopez@gmail.com>
2021-06-29 19:33:36 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_rev - toma la cadena de la funcion.
+ * @s: value taken from main
+ * Return: (0)
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
