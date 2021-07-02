8e09eed25f6536ed6468240381b1d84f3e9e05ff
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-01 18:32:13 -0700

    task 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,4 +10,18 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int b;
+
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[b] = src[b];
+		b++;
+	}
+	while (b < n)
+	{
+		dest[b] = '\0';
+		b++;
+	}
+	return (dest);
 }
