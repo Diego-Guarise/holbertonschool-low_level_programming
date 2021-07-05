b5d3db9cd2d695a4b47a7b1b65fcb3ef18c665b5
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-05 12:34:28 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ * Return: a pointer to a character aka the character found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
