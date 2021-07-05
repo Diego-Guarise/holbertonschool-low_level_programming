ebddd7bfec53d60bc54af26b1d2edf7fa7f5a1ed
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-05 16:00:58 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,3 +10,18 @@
  */
 
 char *_strpbrk(char *s, char *accept)
+{
+	int i, x;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+		for (x = 0; accept[x] != '\0'; x++)
+		{
+			if (s[i] == accept[x])
+				return (&s[i]);
+		}
+	return (NULL);
+}
