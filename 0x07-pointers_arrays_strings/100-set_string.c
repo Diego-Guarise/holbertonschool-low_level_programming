db7fa18f98ac100d6b0d377bd3be11440d32bd1b
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-05 17:53:09 -0700

    task 100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
-/*
+/**
  * set_string - function that sets the value of a pointer to a char
- * @s:  pointer to the adderss of string s
+ * @s:  pointer
  * @to: string to set value to
  * Return: nothing
  */
