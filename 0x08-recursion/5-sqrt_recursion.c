097a1fe90a991e6dc8cff32c79baac4ea185adce
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-08 07:13:02 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+nclude "holberton.h"
+/**
+ * sqtRecursive - computes square root recursively doing binary search
+ * @start: inital number
+ * @end: last number within the limit of number
+ * @m: given number
+ * Return: 1 if not found sqrroot, else sqrroot
+**/
+
+int checker(int i, int n)
+{
+	if (i * i == n)
+		return(i);
+	if (i * i > n)
+		return(-1);
+	return(checker(n, i + ))
+}
+
+int _sqrt_recursion(int n)
+{}
