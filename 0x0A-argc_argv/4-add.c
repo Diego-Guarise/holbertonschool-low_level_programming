48509fedb8cf8ffc5bf827c131359d2e7e94635c
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-13 11:53:40 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * num_checker - checks if a given char is number or not
+ * checker - checks if a given char is number or not
  * @a: char to be checked
  * Return: 1, if its a number, else 0
  **/
@@ -18,7 +18,9 @@ int checker(char *a)
 	while (i < len)
 	{
 		if (a[i] < '0' || a[i] > '9')
+		{
 			return (-1);
+		}
 		else
 			n = n * 10 + (a[i] - '0');
 		i++;
