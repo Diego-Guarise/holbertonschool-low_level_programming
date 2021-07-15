95a098ec8569da397d034d0ee65e8424e2d0f273
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-14 17:44:07 -0700

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * word_count - Count words separate by spaces in a string
+ * @str: String to check
+ *
+ * Return: Number of words;
+ */
+
+int word_count(char *str)
+{
+	if (*str)
+		printf("\n");
+	return (0);
+}
