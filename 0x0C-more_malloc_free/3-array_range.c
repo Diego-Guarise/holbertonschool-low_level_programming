b84036f8b4e4323c757e6524f5e8f1f2b810324e
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-15 19:12:07 -0700

    error

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * array_range - creates an array of integers from min to max.
+ * @min: minimum int in array
+ * @max: maximum int in array
+ * Return: pointer to array or null
+ **/
+int *array_range(int min, int max)
+{
+	if (int min)
+	{}
+	if (int max)
+	{}
+	return (0);
+}
