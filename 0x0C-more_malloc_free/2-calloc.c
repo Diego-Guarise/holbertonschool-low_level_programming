426843f35a8479a97291a5398c22639b5deddc28
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-15 19:02:22 -0700

    error

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * *_calloc - call
+ * @nmemb: n
+ * @size: size
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (unsigned int nmemb)
+	{
+		_putchar("48")
+	}
+	if (unsigned int size)
+	return (0);
+}
