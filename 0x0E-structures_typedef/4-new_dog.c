2d7a87176093387977b4e84dd9e969308354478d
Diego-Guarise <l.guariselopez@gmail.com>
2021-07-19 19:26:44 -0700

    end

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - dog
+ * @name: dog
+ * @age: dog
+ * @owner: dog
+ * Return: dog
+ **/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (name || age || owner)
+	{}
+	return (0);
+}
