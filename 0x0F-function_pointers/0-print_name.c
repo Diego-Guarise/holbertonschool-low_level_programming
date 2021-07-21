2917e6c0ab8e6d89716bc15b1f6b259dd3e18d75
Your Name <you@example.com>
2021-07-21 09:18:55 -0700

    fix 0.2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
-#include <stddef>
+#include <stddef.h>
 
 /**
  * print_name - prints a name
@@ -11,6 +11,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && name != '\0' && f != NULL)
+	if (name != NULL && *name != '\0' && f != NULL)
 	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef HEADERGUARD
 #define HEADERGUARD
-#include <stddef>
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
