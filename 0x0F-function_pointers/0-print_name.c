7ea0599ac55c87ce9bfc532a5fa3b8862acc9af7
Your Name <you@example.com>
2021-07-21 07:07:23 -0700

    Print with fuction

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include "function_pointers.h"
 
-
-/*
- * print_name - funtion
- * name - name
- */
+/**
+ * print_name - prints a name
+ * @name: pointer to char
+ * @f: pointer to function that returns nothing
+ * Return: nothing
+ **/
 
 void print_name(char *name, void (*f)(char *))
 {
-
+	f(name);
 }
