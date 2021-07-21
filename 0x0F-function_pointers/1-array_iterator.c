09d7a42c9ddd406b4abb20b6d60ce2b123d2dfda
Your Name <you@example.com>
2021-07-21 09:33:22 -0700

    fix 0.1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,8 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	i = 0;
-	while (array[i] != 'NULL')
-		i++;
-		action(array[i]);
+	if (array != NULL && size != 0 && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
