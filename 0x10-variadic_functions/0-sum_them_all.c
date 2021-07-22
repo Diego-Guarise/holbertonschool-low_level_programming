a2cc03530059457cbc2760ed0186353222fbde72
Your Name <you@example.com>
2021-07-22 09:49:31 -0700

    end

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all arguements
+ * @n: balb lba bla bla
+ * Return: 0 if n==0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+	int sum;
+
+	va_start(list, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
+}
