c04434d518a4e2916cdd3d9405c774da7ffe3751
Your Name <you@example.com>
2021-07-22 19:44:53 -0700

    xd

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+  * print_strings - print strings
+  * @separator: const char
+  * @n: const unsigned int
+  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	if (separator && n)
+	{}
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print anyting
+ * @format: list of types of arguments
+ * Description: lika printf
+ * Return: the output formated in char, integer, float or char*
+ * string NULL print (nil)
+ * 2 while, 2 if, 9 var and printf
+ */
+
+void print_all(const char * const format, ...)
+{
+	if (format)
+	{}
+}
