9103bce773b3289757f9270b15dcb45acd31fdfb
Your Name <you@example.com>
2021-07-23 10:42:15 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,6 +9,27 @@
   */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	if (separator && n)
-	{}
+	unsigned int i;
+	char *c;
+	va_list list;
+	
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		c = va_arg(list, char *);
+		if (c == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", c);
+		}
+		if (i != (n -1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(list);
+	printf("\n");
 }
