b9413c16ea7a6b2709896321468d9ca18a756704
Your Name <you@example.com>
2021-07-22 19:17:17 -0700

    fix 0.2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,13 +13,9 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-	int sum;
-
-	if (n == 0)
-		return (0);
-
+	int sum = 0;
 	va_start(list, n);
-	sum = 0;
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
