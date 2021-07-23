ba9d78dbfaca536f684853a7a9857384f4daf81c
Your Name <you@example.com>
2021-07-22 19:21:37 -0700

    fix 0.3

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * sum_them_all - sum all arguements
- * @n: balb lba bla bla
- * Return: 0 if n==0
+ *sum_them_all - return the sum of all parameters
+ *@n: number of parameters
+ *Return: sum total
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-	int sum = 0;
+	int sum;
 
 	va_start(list, n);
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0, sum = 0; i < n; i++)
 		sum += va_arg(list, int);
-	}
+
 	va_end(list);
+
 	return (sum);
 }
