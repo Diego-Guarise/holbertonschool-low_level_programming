ef81c41c3333d4c844affbb3010ba00ef233d05a
Your Name <you@example.com>
2021-08-03 07:53:46 -0700

    finish

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_listint - const list_t *h - print
+ * @h: blablalba
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int contador = 0;
+
+	if (h)
+	{
+		if (h->next)
+		{
+			printf("%d\n", h->n);
+			contador++;
+			return (print_listint(h->next) + 1);
+		}
+		else
+		{
+			printf("%d\n", h->n);
+			contador++;
+		}
+	}
+return (contador);
+}
