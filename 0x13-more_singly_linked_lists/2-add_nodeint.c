ecc916807dbc295c911d153688c3f6355cd9abe6
Your Name <you@example.com>
2021-08-03 11:00:50 -0700

    end

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,8 +10,10 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *inicio = NULL;
-
-	if (inicio && head)
+	int i;
+	
+	inicio = malloc(sizeof(listint_t));
+	if (head && inicio)
 	{
 		inicio->n = n;
 		inicio->next = *head;
@@ -21,5 +23,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	else
 	{
 		return (NULL);
+	}
+	for (i = 0; i != '\0'; i++)
+	{
+		printf("%d\n", n);
 	}
 }
