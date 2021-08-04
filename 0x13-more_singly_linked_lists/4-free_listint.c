ab9629782b510ac155e3936d4e51ea53651025b2
Your Name <you@example.com>
2021-08-04 09:47:04 -0700

    end

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * void free_listint - function that frees a listint_t list.
+ * @head: el head
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	if (head)
+	{
+		ptr = head->next;
+		free(head);
+		free_listint(ptr);
+	}
+}
