11754a04b28d78172d7298f6fd9b5e33023186bc
Your Name <you@example.com>
2021-08-04 19:33:47 -0700

    :(

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *delete_nodeint_at_index - a
+ *@head: a
+ *@index: a
+ *Return: a
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	if (head)
+	{
+		return (0);
+	}
+	if (index)
+	{
+		return (0);
+	}
+}
