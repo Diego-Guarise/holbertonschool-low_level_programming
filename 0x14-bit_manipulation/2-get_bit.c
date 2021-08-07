9750e7b7f6167890f50fc18b7b2f36d747f332b6
Your Name <you@example.com>
2021-08-06 19:59:14 -0700

    end

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/** - gets the value of a bit at given index
+  * @n: decimal number, input
+  * @index: index
+  * Return: bit at index, -1 if error
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (n == 0)
+		return (0);
+	for (; i < index; i++)
+	{
+		if (i >= 64)
+			return (-1);
+		n = n >> 1;
+	}
+	return (n & 1);
+}
