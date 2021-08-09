5f5a1a88931665a5ab63c24debe23e8010fdbe13
Your Name <you@example.com>
2021-08-09 09:02:33 -0700

    end

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * ssize_t read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file
+ * @letters: letters
+ * Return: c, write. or 0 if it error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t a, b, c;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	a = open(filename, O_RDONLY);
+	b = read(a, buffer, letters);
+	c = write(1, buffer, b);
+	if ((a == -1) || (b == -1) || (c != b) || (c == -1))
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(a);
+	return (c);
+}
