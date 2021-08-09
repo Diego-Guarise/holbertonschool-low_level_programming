caeb0f4ae1b03d1a27337bb7cfd80dfd869d9e1d
Your Name <you@example.com>
2021-08-09 11:01:36 -0700

    end

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * create_file - Create a function that creates a file.
+  * @filename: file
+  * @text_content: text
+  * Return: 1 if success or -1 if fail
+  */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t o, w, i;
+
+	if (!filename)
+		return (-1);
+	for (i = 0; text_content[i] != '\0'; i++)
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, i);
+	if ((o == -1) || (w == -1))
+		return (-1);
+	close(w);
+	return (1);
+}
