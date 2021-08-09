e8a676fbe18595b404a92a3584a2bd37688ff201
Your Name <you@example.com>
2021-08-09 11:38:28 -0700

    fix 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,9 +14,11 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		return (-1);
 	for (i = 0; text_content[i] != '\0'; i++)
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (o == -1)
+		return (-1);
 	w = write(o, text_content, i);
-	if ((o == -1) || (w == -1))
+	if (w == -1)
 		return (-1);
 	if (w != i)
 	{
