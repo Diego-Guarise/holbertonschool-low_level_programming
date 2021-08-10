182202c46f39ca954bf3cfcf7cead4e82b1c11d8
Your Name <you@example.com>
2021-08-09 19:55:30 -0700

    d

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ *main- cp command
+ *@argc: argument count
+ *@argv: argument list
+ *Return: 0 if no errors
+ */
+int main(int argc, char **argv)
+{
+	if (argc)
+		return (0);
+	if (argv)
+		return (0);
+}
