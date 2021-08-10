0b1e4b6e8ea23e2f592cedb010e03dc0d2b54f8a
Your Name <you@example.com>
2021-08-10 11:13:31 -0700

    end

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,4 +9,39 @@
  */
 int main(int argc, char *argv[])
 {
+	char buffer [1025];
+	ssize_t from, to, c, w;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (-1);
+	}
+	if (!argv[1])
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		return (-1);
+	}
+
+	from = open(argv[1], O_RDONLY);
+	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	c = 1024;
+	while (c == 1024)
+	{
+		c = read(from, buffer, c);
+		w = write(to, buffer, c);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+			return (-1);
+		}
+	}
+	close (from);
+	close (to);
+	if (from == -1 || to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE");
+		return (-1);
+	}
+	return (0);
 }
