c2123ccd7dde57021218542e22e2b1fb2f3ad66c
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-16 09:00:52 -0700

    v1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - return num of elements
+ * @h: lista
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+const dlistint_t *run;
+int nodes = 0;
+
+run = h;
+while (run)
+{
+run = run->next;
+nodes++;
+}
+return (nodes);
+}
