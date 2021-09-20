b4d799afa54828ab4ef9952c6548344e45fff2e8
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-20 10:31:37 -0700

    v1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-gcc -c -fPIC *.c
+gcc *.c -c -fPIC
 gcc *.o -shared -o liball.so
-gcc -Wall -pedantic -Werror -Wextra -L. -ldynamic -o len
-ldd len 
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
-ldd len
-./len
