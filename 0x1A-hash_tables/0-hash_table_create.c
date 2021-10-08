72e178807879ed2e07b69d384a35c9b9f27ff994
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-08 07:45:58 -0700

    v1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -25,5 +25,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	table->size = size;
+
 	return (table);
 }
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,2 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht: the hash table you want to look into
+ * @key: the key you are looking for
+ * Return:  the value associated with the element,
+ * or NULL if key couldn’t be found
+*/
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int i;
+	hash_node_t *tmp;
+
+	if (!ht)
+		return (NULL);
+	i = 0;
+	while (tmp)
+	{
+		if (key == tmp->key)
+		{
+			return (tmp->value);
+		}
+		tmp = ht->array[i];
+		i++;
+	}
+	return (NULL);
+}
