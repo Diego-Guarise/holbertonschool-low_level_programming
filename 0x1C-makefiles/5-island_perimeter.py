917f25b098bba10973ff89f117d277db33714f68
Diego-Guarise <l.guariselopez@gmail.com>
2021-11-25 16:58:47 -0800

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+	Create a function def island_perimeter(grid): that
+	returns the perimeter of the island described in grid:
+"""
+
+def island_perimeter(grid):
+    """
+		grid is a list of list of integers
+    """
+    perimetro = 0
+    for fila in range(len(grid)):
+        for colu in range(len(grid[fila])):
+            if grid[fila][colu] == 1:
+                if fila == 0 or grid[fila - 1][colu] == 0:
+                    perimetro += 1
+                if colu == 0 or grid[fila][colu - 1] == 0:
+                    perimetro += 1
+                if fila == (len(grid) - 1) or grid[fila + 1][colu] == 0:
+                    perimetro += 1
+                if colu == (len(grid[fila]) - 1) or grid[fila][colu + 1] == 0:
+                    perimetro += 1
+    return (perimetro)
