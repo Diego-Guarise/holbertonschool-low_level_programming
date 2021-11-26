d14cf2a42dff594344184291d88dacc2f42b8902
Diego-Guarise <l.guariselopez@gmail.com>
2021-11-25 17:00:43 -0800

    pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-	Create a function def island_perimeter(grid): that
-	returns the perimeter of the island described in grid:
+Create a function def island_perimeter(grid): that
+returns the perimeter of the island described in grid:
 """
 
+
 def island_perimeter(grid):
-    """
-		grid is a list of list of integers
-    """
+    """grid is a list of list of integers"""
     perimetro = 0
     for fila in range(len(grid)):
         for colu in range(len(grid[fila])):
