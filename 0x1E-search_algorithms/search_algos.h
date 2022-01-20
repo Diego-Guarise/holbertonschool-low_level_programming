450d08c9cbd59252710bb69c42d9c00d2867e564
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-19 21:37:59 -0300

    fix includes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,8 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 int linear_search(int *array, size_t size, int value);
